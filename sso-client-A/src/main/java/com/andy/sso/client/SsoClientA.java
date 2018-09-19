00fa41bc6f9b34f7045c3c73394f8b711dea7ea7
leone <exklin@gmail.com>
2018-09-19 18:57:19 +0800

    docs(all):update

--- sso-client-A/src/main/java/com/andy/sso/client/SsoClientA.java
+++ sso-client-A/src/main/java/com/andy/sso/client/SsoClientA.java
@@ -1,31 +1,32 @@
 package com.andy.sso.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * @author Leone
- * @since 2018-05-07 21:25
+ * @since 2018-05-07
  **/
 @RestController
 @EnableOAuth2Sso
 @SpringBootApplication
-public class SSOClientA {
+public class SsoClientA {
 
 //    @Autowired
 //    private OAuth2RestTemplate oAuth2RestTemplate;
 
     public static void main(String[] args) {
-        SpringApplication.run(SSOClientA.class, args);
+        SpringApplication.run(SsoClientA.class, args);
+    }
+
+    //自定义一个RequestContextListener
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 
 //    @GetMapping("/user")
