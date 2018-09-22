f955be1e48aa63e53f99ed9313b1d3d04befc7c7
leone <exklin@gmail.com>
2018-09-22 17:46:06 +0800

    docs(all):update

--- oauth-server/src/main/java/com/andy/oauth/config/AuthServerConfig.java
+++ oauth-server/src/main/java/com/andy/oauth/config/AuthServerConfig.java
@@ -1,14 +1,10 @@
 package com.andy.oauth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -19,10 +15,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  * @author Leone
  * @since 2018-09-19
  **/
-//@Configuration
-//@EnableAuthorizationServer
-//public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
-//
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
 //    @Autowired
 //    private AuthenticationManager authenticationManager;
 //
@@ -31,53 +27,61 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 //        oauthServer.tokenKeyAccess("permitAll()")           //能够获取token的
 //                .checkTokenAccess("isAuthenticated()");     //检测是否认证
 //    }
-//
-//
-////    @Override
-////    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-////        clients.inMemory()
-////                .withClient("client1")
-////                .secret("client1")
-////                .authorizedGrantTypes("authorization_code", "refresh_token")
-////                .scopes("all")
-////                .and()
-////                .withClient("client2")
-////                .secret("client2")
-////                .authorizedGrantTypes("authorization_code", "refresh_token")
-////                .scopes("all");
-////    }
-//
+
+
 //    @Override
-//    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 //        clients.inMemory()
-//                .withClient("SampleClientId")               //接受的clientId
-//                .secret("secret")                           //默认secret
-//                .authorizedGrantTypes("authorization_code") //授权模式
-//                .scopes("user_info")                        //范围，只获取用户认证。
-//                .autoApprove(true);
+//                .withClient("client1")
+//                .secret("client1")
+//                .authorizedGrantTypes("authorization_code", "refresh_token")
+//                .scopes("all")
+//                .and()
+//                .withClient("client2")
+//                .secret("client2")
+//                .authorizedGrantTypes("authorization_code", "refresh_token")
+//                .scopes("all");
 //    }
-//
-////    @Override
-////    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-////        endpoints.tokenStore(jwtTokenStore()).accessTokenConverter(jwtAccessTokenConverter());
-////    }
-//
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientA")                       //clientId
+                .secret("secretA")                           //secret
+                .authorizedGrantTypes("authorization_code")  //授权模式
+                .scopes("all")                               //范围，只获取用户认证。
+                .and()
+                .withClient("clientB")
+                .secret("secretB")
+                .authorizedGrantTypes("authorization_code")
+                .scopes("all")
+//                .autoApprove(true)
+        ;
+
+
+    }
+
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.tokenStore(jwtTokenStore()).accessTokenConverter(jwtAccessTokenConverter());
+//    }
+
 //    @Override
 //    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 //        endpoints.authenticationManager(authenticationManager);
 //    }
-//
-////    @Bean
-////    public TokenStore jwtTokenStore() {
-////        return new JwtTokenStore(jwtAccessTokenConverter());
-////    }
-//
-////    @Bean
-////    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-////        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-////        // 保证JWT安全的唯一方式
-////        jwtAccessTokenConverter.setSigningKey("ZPW");
-////        return jwtAccessTokenConverter;
-////    }
-//
-//}
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        // 保证JWT安全 类似签名
+        jwtAccessTokenConverter.setSigningKey("hello");
+        return jwtAccessTokenConverter;
+    }
+
+}
---
+++ oauth-server/src/main/java/com/andy/oauth/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.andy.oauth.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+
+    private CorsConfiguration buildConfig() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1
+        corsConfiguration.addAllowedOrigin("*");
+        // 2
+        corsConfiguration.addAllowedHeader("*");
+        // 3
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // step 4
+        source.registerCorsConfiguration("/**", buildConfig());
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
