ed1d7ff5a9ff9eb279890d64fcab5816136b6f18
leone <exklin@gmail.com>
2018-10-31 12:55:32 +0800

    feat(all):update resource server

--- sso-resource-server/src/main/java/com/andy/resource/ResourceServerApplication.java
+++ sso-resource-server/src/main/java/com/andy/resource/ResourceServerApplication.java
@@ -3,6 +3,9 @@ package com.andy.resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -10,12 +13,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @author Leone
  * @since 2018-09-19
  **/
-@EnableResourceServer
+@RestController
 @SpringBootApplication
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+    @GetMapping("/api/{id}")
+    public String get(@PathVariable("id") String id) {
+        System.out.println("id=" + id);
+        return "hello resource";
     }
 
 }
