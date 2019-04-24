74df2c857904be7e4b816e4a67df517e6c55bcc6
leone <exklin@gmail.com>
2019-04-24 11:18:35 +0800

    :sparkles: update

---
+++ sso-oauth-server/src/main/java/com/leone/sso/server/service/UserService.java
@@ -0,0 +1,13 @@
+package com.leone.sso.server.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * <p> 后期在此新增UserService的业务接口
+ *
+ * @author leone
+ * @since 2019-04-24
+ **/
+public interface UserService extends UserDetailsService {
+
+}
