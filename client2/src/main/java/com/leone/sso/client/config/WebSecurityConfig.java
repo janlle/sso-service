7f1597a8c8b38a5d4fcfdd708dc34a72a5842612
leone <exklin@gmail.com>
2020-06-04 11:57:07 +0800

    Update

---
+++ client2/src/main/java/com/leone/sso/client/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.leone.sso.client.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * <p>
+ *
+ * @author leone
+ * @since 2020-06-04
+ **/
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
